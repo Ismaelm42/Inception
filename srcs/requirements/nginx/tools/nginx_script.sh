5512340b6776449c0d023bbb09e16232be0d8bcd
Ismael <imoro-sa@student.42malaga.com>
2024-05-23 11:02:07 +0200

    dockerfile nginx + script ok

--- srcs/requirements/nginx/tools/nginx_script.sh
+++ srcs/requirements/nginx/tools/nginx_script.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+#creación de las carpetas en las que se va a encontrar la clave y el certificado
+mkdir -p /etc/nginx/ssl/private /etc/ssl/certs/
+
+#creación de las claves y certificados
+openssl req -x509 -nodes -out /etc/ssl/certs/nginx.crt -keyout /etc/nginx/ssl/private/nginx.key -subj "/C=ES/ST=AND/L=Malaga/O=42/OU=42/CN=login.42.fr/UID=imoro-sa"
+
+#acordar los permisos a la carpeta root
+#chmod -R 755 /var/www/html/
+
+#cambiar el propietario y grupo a la carpeta root. www-data nombre de user y group común que se crea para servidores nginx/apache
+#chown -R www-data:www-data /var/www/html/
+
+#autodestrucción
+rm $0
