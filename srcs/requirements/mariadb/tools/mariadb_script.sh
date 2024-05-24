07a3e7580e12b3b5db3d7710852a01ce1b3dce28
Ismael Moro Santos <imoro-sa@c1r12s3.42malaga.com>
2024-05-24 20:45:21 +0200

    rectificacion script mariadb

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -3,16 +3,22 @@
 # inicia mysql en segundo plano
 service mysql start;
 
+# crea la base de datos si no existe
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
+# crea un usuario si no existe con su respectivo password
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
+# atribuye todos los privilegios de la nueva base de datos al usuario creado
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
+# modifica el usuario root y establece el password
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
-mysql -u root -pYOURPASSWORD -e "FLUSH PRIVILEGES;"
+# se reinician los privilegios de forma a que los cambios se hagan efectivos
+mysql -e "FLUSH PRIVILEGES;"
 
+# se apaga mysql
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
 # inicia mysql en primer plano
