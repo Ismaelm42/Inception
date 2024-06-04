e38e8e0aa33379319bc4c0dea7fe67d64207e0fc
Ismael <imoro-sa@student.42malaga.com>
2024-06-04 14:24:28 +0200

    .env creado

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-# inicia mysql en segundo plano
+# inicia mysql en segundo plano. Se puede poner condicional de modo que examine si existe el volumen de la base de datos para realizarlo. Gemartin Inception.
 service mariadb start; # service mysql start;
 
 sleep 10
 
 # crea la base de datos si no existe
-#mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE DATABASE IF NOT EXISTS \`"SQL_DATABASE"\`;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
 # crea un usuario si no existe con su respectivo password
-#mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "CREATE USER IF NOT EXISTS \`"SQL_USER"\`@'localhost' IDENTIFIED BY '"SQL_PASSWORD"';"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
 # atribuye todos los privilegios de la nueva base de datos al usuario creado
-#mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`"SQL_DATABASE"\`.* TO \`"SQL_USER"\`@'%' IDENTIFIED BY '"SQL_PASSWORD"';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
 # modifica el usuario root y establece el password
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '"SQL_ROOT_PASSWORD"';"
+mysql -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
 # se reinician los privilegios de forma a que los cambios se hagan efectivos
-#mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
-mysql -u root -p"SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
 # se apaga mysql
-#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-mysqladmin -u root -p"SQL_ROOT_PASSWORD" shutdown
-
-# # inicia mysql en primer plano // no es necesario ya que se realiza en CMD ya de por sí
-# exec mysqld_safe
+mysqladmin -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} shutdown
--- srcs/requirements/nginx/tools/nginx_script.sh
+++ srcs/requirements/nginx/tools/nginx_script.sh
@@ -4,7 +4,7 @@
 mkdir -p /etc/nginx/ssl/private /etc/ssl/certs/
 
 #creación de las claves y certificados
-openssl req -x509 -nodes -out /etc/ssl/certs/nginx.crt -keyout /etc/nginx/ssl/private/nginx.key -subj "/C=ES/ST=AND/L=Malaga/O=42/OU=42/CN=login.42.fr/UID=imoro-sa"
+openssl req -x509 -nodes -out /etc/ssl/certs/nginx.crt -keyout /etc/nginx/ssl/private/nginx.key -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN/UID=$UID"
 
 # acordar los permisos a la carpeta root
 chmod -R 755 /var/www/html/
